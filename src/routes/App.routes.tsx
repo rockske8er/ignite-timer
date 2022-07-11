26a960a911ed4b940817ecc0dac9d53e2d74b666
markus90souza <rockske8er@gmail.com>
2022-07-11 19:47:19 -0300

    feat(components): create main layout and header component

--- src/routes/App.routes.tsx
+++ src/routes/App.routes.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
+import { MainLayout } from '../layout/MainLayout'
 import { History } from '../pages/History'
 import { Home } from '../pages/Home'
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path={'/'} element={<Home />} />
-      <Route path={'/history'} element={<History />} />
+      <Route path={'/'} element={<MainLayout />}>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/history'} element={<History />} />
+      </Route>
     </Routes>
   )
 }
