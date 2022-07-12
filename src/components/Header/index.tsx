3d778dc8349f48d2b7533fd8618604131525645b
markus90souza <rockske8er@gmail.com>
2022-07-12 00:12:13 -0300

    style(component): create style from header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,21 @@
-import { Container } from './styles'
+import { NavLink } from 'react-router-dom'
+import { HeaderContainer } from './styles'
 
+import logoIgnite from './../../assets/ignite-logo.svg'
+import { Scroll, Timer } from 'phosphor-react'
 export function Header() {
   return (
-    <Container>
-      <h1>Header</h1>
-    </Container>
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+
+      <nav>
+        <NavLink to={'/'}>
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to={'/history'}>
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,29 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export const Container = styled.div`
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-items: center;
+  justify-content: space-between;
 
-`;
+  nav {
+    display: flex;
+    gap: 0.5rem;
+    a {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 3rem;
+      height: 3rem;
+      color: ${({ theme }) => theme.colors['gray-100']};
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+      &:hover {
+        border-bottom: 3px solid ${({ theme }) => theme.colors['green-500']};
+      }
+
+      &.active {
+        color: ${({ theme }) => theme.colors['green-500']};
+      }
+    }
+  }
+`
