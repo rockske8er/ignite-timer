7bd0d063888ebbec25c6cced6ed3f861c2e92dd3
markus90souza <rockske8er@gmail.com>
2022-07-11 23:35:13 -0300

    style(component): add style on MainLayout component

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
 
-import { Container } from './styles'
+import { LayoutContainer } from './styles'
 
 export function MainLayout() {
   return (
-    <Container>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </Container>
+    </LayoutContainer>
   )
 }
--- src/layout/MainLayout/styles.ts
+++ src/layout/MainLayout/styles.ts
@@ -1,3 +1,13 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const LayoutContainer = styled.div`
+  width: 100%;
+  max-width: 74rem;
+  height: calc(100vh - 10rem);
+  margin: 5rem auto;
+  background-color: ${({ theme }) => theme.colors['gray-800']};
+  border-radius: 0.5rem;
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+`
